bf86fce43401c967ba536230a8518d7caf4ac841
Rajdeep Dey <rajdeep.dey.fiem.bca23@teamfuture.in>
2026-03-06 10:03:37 +0530

    Fix syntax error in visual ads screen by correcting list closure

--- lib/screens/visual_ads/visual_ads_screen.dart
+++ lib/screens/visual_ads/visual_ads_screen.dart
@@ -238,7 +238,7 @@ class _VisualAdsScreenState extends ConsumerState<VisualAdsScreen> {
                                 ),
                               ),
                             );
-                          }).toList(),
+                          }),
                         ],
                         onChanged: (value) {
                           ref.read(filteredVisualAdsProvider.notifier).state = value;
