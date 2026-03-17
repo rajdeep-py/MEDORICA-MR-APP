7634e8b196f05b110a67962a6e621b39c72435d2
Rajdeep Dey <rajdeep.dey.fiem.bca23@teamfuture.in>
2026-03-17 14:52:23 +0530

    main.dart to integrate AuthState listener and remove unused authMrIdListenerProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mr_app/notifiers/auth_notifier.dart';
+import 'package:mr_app/provider/gift_provider.dart';
 import 'router/app_router.dart';
 import 'theme/app_theme.dart';
+import 'provider/auth_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,21 +17,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PopScope(
-      canPop: false,
-      onPopInvokedWithResult: (didPop, result) {
-        if (didPop) {
-          return;
-        }
-        // Prevent app from exiting - navigate to home instead
-        AppRouter.router.go(AppRouter.home);
+    return Consumer(
+      builder: (context, ref, _) {
+        ref.listen<AuthState>(authNotifierProvider, (previous, next) {
+          final mrId = next.mr?.mrId;
+          ref.read(currentMrIdProvider.notifier).state = mrId;
+        });
+        return PopScope(
+          canPop: false,
+          onPopInvokedWithResult: (didPop, result) {
+            if (didPop) {
+              return;
+            }
+            // Prevent app from exiting - navigate to home instead
+            AppRouter.router.go(AppRouter.home);
+          },
+          child: MaterialApp.router(
+            title: 'Medorica MR App',
+            debugShowCheckedModeBanner: false,
+            theme: AppTheme.lightTheme,
+            routerConfig: AppRouter.router,
+          ),
+        );
       },
-      child: MaterialApp.router(
-        title: 'Medorica MR App',
-        debugShowCheckedModeBanner: false,
-        theme: AppTheme.lightTheme,
-        routerConfig: AppRouter.router,
-      ),
     );
   }
 }
--- lib/provider/auth_provider.dart
+++ lib/provider/auth_provider.dart
@@ -2,7 +2,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/legacy.dart';
 import '../notifiers/auth_notifier.dart';
 import '../services/auth/auth_services.dart';
-import 'gift_provider.dart';
 
 final authServiceProvider = Provider<AuthService>((ref) {
   return AuthService();
@@ -14,9 +13,3 @@ final authNotifierProvider = StateNotifierProvider<AuthNotifier, AuthState>((
   return AuthNotifier(ref.read(authServiceProvider));
 });
 
-// Listen to AuthState and set currentMrIdProvider
-final authMrIdListenerProvider = Provider<void>((ref) {
-  final authState = ref.watch(authNotifierProvider);
-  final mrId = authState.mr?.mrId; // Assumes AuthState has mrId field
-  ref.read(currentMrIdProvider.notifier).state = mrId;
-});
